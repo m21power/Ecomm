646874b24ebf6ac3d812a2bd490551b5cf093168
Mesay <mesaylema21@gmail.com>
2024-10-23 14:29:07 +0300

    order and order item with their respective method added

--- ecomm-api/storer/storer_mysql.go
+++ ecomm-api/storer/storer_mysql.go
@@ -61,3 +61,134 @@ func (ms *MySQLStorer) DeleteProduct(ctx context.Context, id int64) error {
 	}
 	return nil
 }
+
+// transaction to create order and order items
+func (ms *MySQLStorer) CreateOrder(ctx context.Context, o *Order) (*Order, error) {
+	err := ms.execTx(ctx, func(tx *sqlx.Tx) error {
+		// Insert into orders
+		order, err := ms.createOrder(ctx, tx, o)
+		if err != nil {
+			return fmt.Errorf("error creating order: %w", err)
+		}
+		// Insert order items
+		for _, oi := range o.Items {
+			oi.OrderID = order.ID
+			id, err := ms.createOrderItem(ctx, tx, &oi)
+			if err != nil {
+				return fmt.Errorf("error creating order item: %w", err)
+			}
+			oi.ID = id
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error creating order: %w", err)
+	}
+
+	// Return the created order with its items
+	return o, nil
+}
+
+func (ms *MySQLStorer) createOrder(ctx context.Context, tx *sqlx.Tx, o *Order) (*Order, error) {
+	res, err := tx.NamedExecContext(ctx, "INSERT INTO orders (payment_method, tax_price, shipping_price, total_price) VALUES (:payment_method, :tax_price, :shipping_price, :total_price)", o)
+	if err != nil {
+		return nil, fmt.Errorf("error inserting order: %w", err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return nil, fmt.Errorf("error getting last insert ID: %w", err)
+	}
+	o.ID = id
+	return o, nil
+}
+
+func (ms *MySQLStorer) createOrderItem(ctx context.Context, tx *sqlx.Tx, oi *OrderItem) (int64, error) {
+	res, err := tx.NamedExecContext(ctx, "INSERT INTO order_items (name,quantity,image,price,product_id,order_id) VALUES (:name,:quantity,:image,:price,:product_id,:order_id)", oi)
+	if err != nil {
+		return 0, fmt.Errorf("error inserting order item: %w", err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("error getting last insert ID: %w", err)
+	}
+	return id, nil
+
+}
+
+func (ms *MySQLStorer) GetOrder(ctx context.Context, id int64) (*Order, error) {
+	var o Order
+	err := ms.db.GetContext(ctx, &o, "SELECT * FROM orders WHERE id=?", id)
+	if err != nil {
+		return nil, fmt.Errorf("error getting order: %w", err)
+	}
+	var oi []OrderItem
+	err = ms.db.SelectContext(ctx, &oi, "SELECT * FROM order_items WHERE order_id=?", id)
+	if err != nil {
+		return nil, fmt.Errorf("error getting order items: %w", err)
+	}
+	o.Items = oi
+	return &o, nil
+}
+
+func (ms *MySQLStorer) ListOrders(ctx context.Context) ([]Order, error) {
+	var orders []Order
+	err := ms.db.SelectContext(ctx, &orders, "SELECT * FROM orders")
+	if err != nil {
+		return nil, fmt.Errorf("error listing orders: %w", err)
+	}
+	for i := range orders {
+		var oi []OrderItem
+		err = ms.db.SelectContext(ctx, &oi, "SELECT * FROM order_items WHERE order_id=?", orders[i].ID)
+		if err != nil {
+			return nil, fmt.Errorf("error getting order items: %w", err)
+		}
+		orders[i].Items = oi
+	}
+	return orders, nil
+}
+
+// updata order items
+// delete order and order items
+func (ms *MySQLStorer) DeleteOrder(ctx context.Context, id int64) error {
+	err := ms.execTx(ctx, func(tx *sqlx.Tx) error {
+		_, err := tx.ExecContext(ctx, "DELETE FROM order_items WHERE order_id=?", id)
+		if err != nil {
+			return fmt.Errorf("error deleting order_items: %w", err)
+		}
+		_, err = tx.ExecContext(ctx, "DELETE FROM order WHERE id=?", id)
+		if err != nil {
+			return fmt.Errorf("error deleting order: %w", err)
+		}
+		return nil
+
+	})
+	if err != nil {
+		return fmt.Errorf("error deleting order: %w", err)
+	}
+	return nil
+}
+func (ms *MySQLStorer) execTx(ctx context.Context, fn func(*sqlx.Tx) error) error {
+	// Begin the transaction
+	tx, err := ms.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("error beginning transaction: %w", err)
+	}
+
+	// Execute the transaction function
+	err = fn(tx)
+	if err != nil {
+		// Attempt to rollback if error occurs
+		if rberr := tx.Rollback(); rberr != nil {
+			return fmt.Errorf("error rolling back transaction: %w (original error: %s)", rberr, err)
+		}
+		return fmt.Errorf("error in transaction: %w", err)
+	}
+
+	// Commit the transaction
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("error committing transaction: %w", err)
+	}
+
+	return nil
+}
--- ecomm-api/storer/storer_mysql_test.go
+++ ecomm-api/storer/storer_mysql_test.go
@@ -299,3 +299,294 @@ func TestDeleteProduct(t *testing.T) {
 		})
 	}
 }
+
+func TestCreateOrder(t *testing.T) {
+	ois := []OrderItem{
+		{
+			Name:      "product 1",
+			Quantity:  1,
+			Image:     "test.jpg",
+			Price:     99.99,
+			ProductID: 1,
+		},
+		{
+			Name:      "product 2",
+			Quantity:  2,
+			Image:     "test2.jpg",
+			Price:     99.99,
+			ProductID: 2,
+		},
+	}
+	o := &Order{
+		PaymentMethod: "test payment method",
+		TaxPrice:      34,
+		ShippingPrice: 123,
+		TotalPrice:    1235,
+		Items:         ois,
+	}
+
+	tcs := []struct {
+		name string
+		test func(*testing.T, *MySQLStorer, sqlmock.Sqlmock)
+	}{
+		{
+			name: "success",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				// Start the transaction
+				mock.ExpectBegin()
+
+				// Mock order insertion
+				mock.ExpectExec("INSERT INTO orders (payment_method, tax_price, shipping_price, total_price) VALUES (?, ?, ?, ?)").WithArgs(o.PaymentMethod, o.TaxPrice, o.ShippingPrice, o.TotalPrice).WillReturnResult(sqlmock.NewResult(1, 1))
+
+				// Mock first order item insertion (order_id = 1)
+				mock.ExpectExec("INSERT INTO order_items (name,quantity,image,price,product_id,order_id) VALUES (?,?,?,?,?,?)").WithArgs(ois[0].Name, ois[0].Quantity, ois[0].Image, ois[0].Price, ois[0].ProductID, 1).WillReturnResult(sqlmock.NewResult(1, 1))
+
+				// Mock second order item insertion (order_id = 1)
+				mock.ExpectExec("INSERT INTO order_items (name,quantity,image,price,product_id,order_id) VALUES (?,?,?,?,?,?)").WithArgs(ois[1].Name, ois[1].Quantity, ois[1].Image, ois[1].Price, ois[1].ProductID, 1).WillReturnResult(sqlmock.NewResult(2, 1))
+
+				// Commit the transaction
+				mock.ExpectCommit()
+
+				// Execute the function
+				cp, err := st.CreateOrder(context.Background(), o)
+				require.NoError(t, err)
+				require.Equal(t, int64(1), cp.ID)
+
+				// Verify all expectations were met
+				er := mock.ExpectationsWereMet()
+				require.NoError(t, er)
+			},
+		},
+		{
+			name: "failure_rollback",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				// Start the transaction
+				mock.ExpectBegin()
+
+				// Mock order insertion failure
+				mock.ExpectExec("INSERT INTO orders (payment_method, tax_price, shipping_price, total_price) VALUES (?, ?, ?, ?)").WithArgs(
+					o.PaymentMethod, o.TaxPrice, o.ShippingPrice, o.TotalPrice,
+				).WillReturnError(fmt.Errorf("db error"))
+
+				// Expect rollback
+				mock.ExpectRollback()
+
+				// Execute the function
+				_, err := st.CreateOrder(context.Background(), o)
+
+				// Expect an error and a rollback
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "db error")
+
+				// Verify all expectations were met
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+				st := NewMySQLStorer(db)
+				tc.test(t, st, mock)
+			})
+		})
+	}
+}
+
+func TestGetOrder(t *testing.T) {
+	ois := []OrderItem{
+		{
+			ID:        1,
+			Name:      "product 1",
+			Quantity:  1,
+			Image:     "test.jpg",
+			Price:     99.99,
+			ProductID: 1,
+			OrderID:   1,
+		},
+		{
+			ID:        2,
+			Name:      "product 2",
+			Quantity:  2,
+			Image:     "test2.jpg",
+			Price:     99.99,
+			ProductID: 2,
+			OrderID:   1,
+		},
+	}
+	o := &Order{
+		ID:            1,
+		PaymentMethod: "test payment method",
+		TaxPrice:      34,
+		ShippingPrice: 123,
+		TotalPrice:    1235,
+		Items:         ois,
+	}
+	tcs := []struct {
+		name string
+		test func(*testing.T, *MySQLStorer, sqlmock.Sqlmock)
+	}{
+		{
+			name: "success",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				// Mock the order query
+				rows := sqlmock.NewRows([]string{"id", "payment_method", "tax_price", "shipping_price", "total_price", "created_at", "updated_at"}).
+					AddRow(o.ID, o.PaymentMethod, o.TaxPrice, o.ShippingPrice, o.TotalPrice, o.CreatedAt, o.UpdatedAt)
+				mock.ExpectQuery("SELECT * FROM orders WHERE id=?").WithArgs(1).WillReturnRows(rows)
+
+				// Mock the order items query
+				rows = sqlmock.NewRows([]string{"id", "name", "quantity", "image", "price", "product_id", "order_id"}).
+					AddRow(ois[0].ID, ois[0].Name, ois[0].Quantity, ois[0].Image, ois[0].Price, ois[0].ProductID, ois[0].OrderID)
+				mock.ExpectQuery("SELECT * FROM order_items WHERE order_id=?").WithArgs(1).WillReturnRows(rows)
+				gp, err := st.GetOrder(context.Background(), 1)
+				require.NoError(t, err)
+				require.Equal(t, int64(1), gp.ID)
+			}},
+		{
+			name: "error getting order",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("SELECT * FROM orders WHERE id=?").WithArgs(1).WillReturnError(fmt.Errorf("error getting order"))
+				_, err := st.GetOrder(context.Background(), 1)
+				require.Error(t, err)
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			}},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+				st := NewMySQLStorer(db)
+				tc.test(t, st, mock)
+			})
+		})
+	}
+}
+
+func TestListOrders(t *testing.T) {
+	ois := []OrderItem{
+		{
+			ID:        1,
+			Name:      "product 1",
+			Quantity:  1,
+			Image:     "test.jpg",
+			Price:     99.99,
+			ProductID: 1,
+			OrderID:   1,
+		},
+		{
+			ID:        2,
+			Name:      "product 2",
+			Quantity:  2,
+			Image:     "test2.jpg",
+			Price:     99.99,
+			ProductID: 2,
+			OrderID:   1,
+		},
+	}
+	o := &Order{
+		ID:            1,
+		PaymentMethod: "test payment method",
+		TaxPrice:      34,
+		ShippingPrice: 123,
+		TotalPrice:    1235,
+		Items:         ois,
+	}
+	tcs := []struct {
+		name string
+		test func(*testing.T, *MySQLStorer, sqlmock.Sqlmock)
+	}{
+		{
+			name: "success",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				// Mock the orders query
+				rows := sqlmock.NewRows([]string{"id", "payment_method", "tax_price", "shipping_price", "total_price", "created_at", "updated_at"}).
+					AddRow(o.ID, o.PaymentMethod, o.TaxPrice, o.ShippingPrice, o.TotalPrice, o.CreatedAt, o.UpdatedAt)
+				mock.ExpectQuery("SELECT * FROM orders").WillReturnRows(rows)
+
+				// Mock the order items query
+				rows = sqlmock.NewRows([]string{"id", "name", "quantity", "image", "price", "product_id", "order_id"}).
+					AddRow(ois[0].ID, ois[0].Name, ois[0].Quantity, ois[0].Image, ois[0].Price, ois[0].ProductID, ois[0].OrderID).
+					AddRow(ois[1].ID, ois[1].Name, ois[1].Quantity, ois[1].Image, ois[1].Price, ois[1].ProductID, ois[1].OrderID)
+
+				mock.ExpectQuery("SELECT * FROM order_items WHERE order_id=?").WithArgs(1).WillReturnRows(rows)
+				lo, err := st.ListOrders(context.Background())
+				require.NoError(t, err)
+				require.Len(t, lo, 1)
+				require.Equal(t, int64(1), lo[0].ID)
+				require.Len(t, lo[0].Items, 2)
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			}},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+				st := NewMySQLStorer(db)
+				tc.test(t, st, mock)
+			})
+		})
+	}
+}
+
+func TestDeleteOrder(t *testing.T) {
+
+	tcs := []struct {
+		name string
+		test func(*testing.T, *MySQLStorer, sqlmock.Sqlmock)
+	}{
+		{
+			name: "success",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				// Start the transaction
+				mock.ExpectBegin()
+
+				// Mock the order items deletion
+				mock.ExpectExec("DELETE FROM order_items WHERE order_id=?").WithArgs(1).WillReturnResult(sqlmock.NewResult(1, 1))
+				// Mock the order deletion
+				mock.ExpectExec("DELETE FROM order WHERE id=?").WithArgs(1).WillReturnResult(sqlmock.NewResult(1, 1))
+
+				// Commit the transaction
+				mock.ExpectCommit()
+
+				// Execute the function
+				err := st.DeleteOrder(context.Background(), 1)
+				require.NoError(t, err)
+
+				// Verify all expectations were met
+				er := mock.ExpectationsWereMet()
+				require.NoError(t, er)
+			}},
+		{
+			name: "failure_rollback",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				// Start the transaction
+				mock.ExpectBegin()
+
+				// Mock order deletion failure
+				mock.ExpectExec("DELETE FROM order_items WHERE order_id=?").WithArgs(1).WillReturnError(fmt.Errorf("error deleting order"))
+
+				// Expect rollback
+				mock.ExpectRollback()
+
+				// Execute the function
+				err := st.DeleteOrder(context.Background(), 1)
+
+				// Expect an error and a rollback
+				require.Error(t, err)
+
+				// Verify all expectations were met
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			}},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+				st := NewMySQLStorer(db)
+				tc.test(t, st, mock)
+			})
+		})
+	}
+}
