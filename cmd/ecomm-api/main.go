8bdfedeeb92019d03f4fa5cfd3268fb1c32783b1
Mesay <mesaylema21@gmail.com>
2024-10-23 23:27:22 +0300

    http handler added for products

--- cmd/ecomm-api/main.go
+++ cmd/ecomm-api/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 
 	"github.com/m21power/ecomm/db"
+	"github.com/m21power/ecomm/ecomm-api/handler"
+	"github.com/m21power/ecomm/ecomm-api/server"
+	"github.com/m21power/ecomm/ecomm-api/storer"
 )
 
 func main() {
@@ -13,5 +16,13 @@ func main() {
 	}
 	defer db.Close()
 	log.Printf("Connected to database")
-
+	st := storer.NewMySQLStorer(db.GetDB())
+	server := server.NewServer(st)
+	h := handler.NewHandler(server)
+	handler.RegisterRoutes(h)
+	log.Printf("Starting server on :8080")
+	err = handler.Start(":8080")
+	if err != nil {
+		log.Fatalf("error starting server: %v", err)
+	}
 }
